516b38bdbc7ab5b6f5161f8bcdc9209d330e0dc1
G.G.D Sreehith <sreehithgrandhi>
2026-06-16 13:57:07 +0530

    Add start script for Railway deployment

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use("/api/profiles", profileRoutes);
 
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
